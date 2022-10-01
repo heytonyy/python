81be3b375b12650b988ad7c8425aa114386bf1ac
Tony Aiello <heytonyyy3@gmail.com>
2022-10-01 10:49:43 -0700

    updated again

--- flask/great_number_game/server.py
+++ flask/great_number_game/server.py
@@ -7,9 +7,31 @@ app.secret_key = 'FLY YOU FOOLS'
 
 @app.route('/')
 def index():
-    session.clear()
+    if 'winners' not in session:
+        session['winners'] = []
     the_number = random.randint(1,100)
     session['answer'] = the_number
+    session['attempts'] = 0
+    session['guess'] = ''
+    session['result'] = ''
+    return render_template('index.html')
+
+@app.post('/leaderboard')
+def add_to_board():
+    # could'nt append to session['winners'], so had to make a var to temp hold
+    # the winners list, append to that, them set temp list to session['winners']
+    session_winners = session['winners']
+    session_winners.append(
+        {
+            'name' : request.form['new_winner'],
+            'attempts' : session['attempts']
+        })
+    session['winners'] = session_winners
+    return redirect('/leaderboard')
+
+@app.get('/leaderboard')
+def added_to_board():
+    session['show_add_name'] = False
     return render_template('index.html')
 
 @app.post('/guess')
@@ -18,10 +40,13 @@ def take_guess():
     print(session)
     if session['guess'] == session['answer']:
         session['result'] = 'correct'
+        session['show_add_name'] = True
     elif session['guess'] < session['answer']:
+        session['attempts'] += 1
         session['result'] = 'too_low'
     else:
         session['result'] = 'too_high'
+        session['attempts'] += 1
     return redirect('/guess')
 
 @app.get('/guess')
